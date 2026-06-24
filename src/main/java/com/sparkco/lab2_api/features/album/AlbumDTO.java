f9c32acb7443d67089b10c7b90f5abd8fcd9d8d2
James Sparks <ru_sparks@yahoo.com>
2026-06-24 11:46:45 -0700

    AlbumDTO to Record

--- src/main/java/com/sparkco/lab2_api/features/album/AlbumDTO.java
+++ src/main/java/com/sparkco/lab2_api/features/album/AlbumDTO.java
@@ -1,46 +1,4 @@
 package com.sparkco.lab2_api.features.album;
 
-public class AlbumDTO {
-
-    private Integer albumId;
-    private String title;
-    private Integer artistId;
-
-    public AlbumDTO() {
-    }
-
-    public AlbumDTO(Integer albumId, String title, Integer artistId) {
-        this.albumId = albumId;
-        this.title = title;
-        this.artistId = artistId;
-    }
-
-    public AlbumDTO(String title, Integer artistId) {
-        this.title = title;
-        this.artistId = artistId;
-    }
-
-    public Integer getAlbumId() {
-        return albumId;
-    }
-
-    public void setAlbumId(Integer albumId) {
-        this.albumId = albumId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Integer getArtistId() {
-        return artistId;
-    }
-
-    public void setArtistId(Integer artistId) {
-        this.artistId = artistId;
-    }
+public record AlbumDTO(Integer albumId, String title, Integer artistId) {
 }
